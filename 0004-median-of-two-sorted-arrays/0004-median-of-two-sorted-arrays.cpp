1d4250034180502d4bdf6000d207d5454b6a63d0
Jisan Mahmud <jisa.mahmud20@gmail.com>
2026-08-26 12:22:52 +0600

    Time: 0 ms (100%), Space: 95.1 MB (79.92%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,34 +1,42 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> merged;
-        int i = 0, j = 0;
-        
-        while (i < nums1.size() && j < nums2.size()) {
-            if (nums1[i] < nums2[j]) {
-                merged.push_back(nums1[i]);
-                i++;
-            } else {
-                merged.push_back(nums2[j]);
-                j++;
+
+        if(nums1.size() > nums2.size())
+            return findMedianSortedArrays(nums2, nums1);
+
+        int total = nums1.size() + nums2.size();
+
+        int low = 0;
+        int high = nums1.size();
+
+        while(low <= high){
+
+            int partitionX = (low + high) / 2;
+            int partitionY = (total + 1) / 2 - partitionX;
+
+            int l1 = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+            int r1 = (partitionX == nums1.size()) ? INT_MAX : nums1[partitionX];
+
+            int l2 = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+            int r2 = (partitionY == nums2.size()) ? INT_MAX : nums2[partitionY];
+
+            if(l1 <= r2 && l2 <= r1){
+                if(total % 2 == 1){
+                    return max(l1, l2);
+                }
+                return (max(l1, l2) + min(r1, r2)) / 2.0;
+            }
+
+            if(l1 > r2){
+                high = partitionX - 1;
+            }
+
+            else{
+                low = partitionX + 1;
             }
         }
 
-        while (i < nums1.size()) {
-            merged.push_back(nums1[i]);
-            i++;
-        }
-
-        while (j < nums2.size()) {
-            merged.push_back(nums2[j]);
-            j++;
-        }
-
-        int totalSize = merged.size();
-        if (totalSize % 2 == 1) {
-            return merged[totalSize / 2]; 
-        } else {
-            return (merged[totalSize / 2 - 1] + merged[totalSize / 2]) / 2.0;
-        }
+        return 0.0;
     }
 };
