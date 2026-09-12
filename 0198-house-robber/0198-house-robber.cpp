682d933709820583995a6e650ed1e7cf721cd47a
Jisan Mahmud <jisa.mahmud20@gmail.com>
2026-09-12 21:12:47 +0600

    Time: 0 ms (100%), Space: 9.9 MB (97.53%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,16 +4,16 @@ public:
         int n = nums.size();
         if(n == 1) return nums[0];
 
-        vector<int> dp(n);
-
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        int x = nums[0];
+        int y = max(nums[0], nums[1]);
 
         for(int i = 2; i < n; i++){
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+            int ans = max(y, x + nums[i]);
+            x = y;
+            y = ans;
         }
 
-        return dp[n-1];
+        return y;
         
     }
 };
