e83869400adb42b131da4f300a89ff63feaa9f04
Jisan Mahmud <jisa.mahmud20@gmail.com>
2026-09-14 14:18:48 +0600

    Time: 0 ms (100%), Space: 9.4 MB (54%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -21,11 +21,11 @@ public:
             for(int j = 0; j < n; j++){
                 if(i == 0 and j == 0) continue;
 
-                int left = 0, down = 0;
+                int up = 0, left = 0;
 
-                if(i > 0) down = dp[i-1][j];
+                if(i > 0) up = dp[i-1][j];
                 if(j > 0) left = dp[i][j-1];
-                dp[i][j] = left + down;
+                dp[i][j] = up + left;
             }
         }
 
