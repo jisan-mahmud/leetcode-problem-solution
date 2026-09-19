5a545dc778175c71b72c3c9c8cac683895d2b9de
Jisan Mahmud <jisa.mahmud20@gmail.com>
2026-09-19 12:04:59 +0600

    Time: 0 ms (100%), Space: 13.3 MB (44.35%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -14,7 +14,18 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         vector<vector<int>> dp(n, vector<int>(n, INT_MIN));
+        dp[0][0] = triangle[0][0];
 
-        return solve(triangle, 0, 0, dp);
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                int up = j < i ? dp[i-1][j] : INT_MAX;
+                int dia = j > 0 ? dp[i-1][j-1] : INT_MAX;
+                dp[i][j] = min(up, dia) + triangle[i][j];
+            }
+        }
+
+        int ans = *min_element(dp[n-1].begin(), dp[n-1].end());
+
+        return ans;
     }
 };
