6df8c571eb8da002e973ffbb1be5facbc7c9456f
Jisan Mahmud <jisa.mahmud20@gmail.com>
2026-09-21 02:52:57 +0600

    Time: 14 ms (52.96%), Space: 25.1 MB (33.34%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        unordered_map<int, int> dp;  
-        dp[0] = 1;                 
+
+        int total = 0;
+        for (int x : nums)
+            total += x;
+
+        if (abs(target) > total)
+            return 0;
+
+        int offset = total;
+
+        vector<int> dp(2 * total + 1, 0);
+        dp[offset] = 1;  // sum = 0
 
         for (int x : nums) {
-            unordered_map<int, int> next;
 
-            for (auto& [sum, ways] : dp) {
-                next[sum + x] += ways;   
-                next[sum - x] += ways; 
+            vector<int> next(2 * total + 1, 0);
+
+            for (int sum = -total; sum <= total; sum++) {
+
+                if (dp[sum + offset] == 0)
+                    continue;
+
+                int ways = dp[sum + offset];
+
+                next[sum + x + offset] += ways;
+                next[sum - x + offset] += ways;
             }
 
             dp = next;
         }
 
-        return dp[target];
+        return dp[target + offset];
     }
 };
