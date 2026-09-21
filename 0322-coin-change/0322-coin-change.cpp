48990ceae6acbcad2a676fe124afccef9fa4404d
Jisan Mahmud <jisa.mahmud20@gmail.com>
2026-09-22 02:05:20 +0600

    Time: 13 ms (94.94%), Space: 17.9 MB (83.72%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -23,9 +23,24 @@ public:
 
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<int> dp(amount+1, -1);
+        vector<int> dp(amount+1, INT_MAX - 1);
 
-        int ans = solve(amount, coins, dp);
+        dp[0] = 0;
+
+        for(int a = 1; a <= amount; a++){
+            int ans = INT_MAX - 1;
+
+            for(int i = 0; i < n; i++){
+                if(coins[i] <= a){
+                    ans = min(ans, dp[a - coins[i]] + 1);
+                }
+            }
+
+            dp[a] = ans;
+        }
+
+
+        int ans = dp[amount];
 
         return ans == INT_MAX - 1 ? -1 : ans;
     }
